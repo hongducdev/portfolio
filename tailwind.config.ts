404b892809352d2df1894b20c681d3744cac64d3
hongducdev <contact.hongduc@gmail.com>
2024-03-06 20:41:34 +0700

    Init project with catppuccin tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "latte",
+    }),
+  ],
 };
 export default config;
