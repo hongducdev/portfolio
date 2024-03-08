876f8b5971e50894d5fcd6d788d930906aa7911f
hongducdev <contact.hongduc@gmail.com>
2024-03-08 07:43:18 +0700

    Refactor page component and social component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
 import Information from "@/components/Information";
 import InformationMore from "@/components/InformationMore";
 import Social from "@/components/Social";
 import { socialList } from "@/utils/social";
+import Technologies from "@/components/Technologies";
 
 const HomePage = () => {
   return (
-    <section>
-      <motion.div>
-        <Information />
-      </motion.div>
-      <motion.div>
-        <Social items={socialList} speed="normal" />
-      </motion.div>
-      <motion.div>
-        <InformationMore />
-      </motion.div>
+    <section className="flex flex-col gap-y-2">
+      <Information />
+      <Social items={socialList} speed="normal" />
+      <InformationMore />
+      <Technologies />
     </section>
   );
 };
---
+++ components/Technologies.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { RiReactjsFill } from "react-icons/ri";
+import { BiLogoTailwindCss } from "react-icons/bi";
+import { SiNodedotjs, SiExpress } from "react-icons/si";
+
+const Technologies = () => {
+  const { ref, inView } = useInView();
+
+  return (
+    <section>
+      <motion.div
+        className="bg-ctp-surface0 px-12 py-10 rounded-3xl"
+        ref={ref}
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: inView ? 0 : 50, opacity: inView ? 1 : 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+      >
+        <h3 className="font-medium text-5xl">🧑‍💻the technologies I use.</h3>
+      </motion.div>
+
+      <ul className="mt-2 text-3xl flex flex-col gap-y-2">
+        <div className="flex gap-2">
+          <motion.li
+            className="basis-3/5 bg-ctp-surface0 px-12 py-10 rounded-3xl flex items-center gap-10 justify-between"
+            ref={ref}
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: inView ? 0 : -50, opacity: inView ? 1 : 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <BiLogoTailwindCss className="text-9xl" />
+            <span className="font-medium text-6xl">TailwindCSS</span>
+          </motion.li>
+          <motion.li
+            className="basis-2/5 bg-ctp-surface0 px-12 py-10 rounded-3xl flex items-center gap-10 justify-between"
+            ref={ref}
+            initial={{ x: 50, opacity: 0 }}
+            animate={{ x: inView ? 0 : 50, opacity: inView ? 1 : 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <RiReactjsFill className="text-9xl" />
+            <span className="font-medium text-6xl">ReactJS</span>
+          </motion.li>
+        </div>
+        <div className="flex gap-2">
+          <motion.li
+            className="basis-2/5 bg-ctp-surface0 px-12 py-10 rounded-3xl flex items-center gap-10 justify-between"
+            ref={ref}
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: inView ? 0 : -50, opacity: inView ? 1 : 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <SiNodedotjs className="text-9xl" />
+            <span className="font-medium text-6xl">NodeJS</span>
+          </motion.li>
+          <motion.li
+            className="basis-3/5 bg-ctp-surface0 px-12 py-10 rounded-3xl flex items-center gap-10 justify-between"
+            ref={ref}
+            initial={{ x: 50, opacity: 0 }}
+            animate={{ x: inView ? 0 : 50, opacity: inView ? 1 : 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <SiExpress className="text-9xl" />
+            <span className="font-medium text-6xl">ExpressJS</span>
+          </motion.li>
+        </div>
+      </ul>
+    </section>
+  );
+};
+
+export default Technologies;
