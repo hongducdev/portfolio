89618ac9985e4445485e611441af8c2a98d0e06d
hongducdev <contact.hongduc@gmail.com>
2024-06-28 22:57:49 +0700

    SEO: Update robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import { MetadataRoute } from "next";
+import { getPublishedPosts, getTags } from "@/apis";
+import { Post, Tag } from "@prisma/client";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPublishedPosts();
+  const tags = await getTags();
+
+  const postsEntries = posts.map((post: Post) => ({
+    url: `${process.env.BASE_URL}/post/${post.slug}`,
+    lastModified: post.updatedAt,
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }));
+
+  const tagsEntries = tags.map((tag: Tag) => ({
+    url: `${process.env.BASE_URL}/tag/${tag.tagName}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.5,
+  }));
+
+  return [
+    {
+      url: `${process.env.BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...postsEntries,
+    ...tagsEntries,
+  ];
+}
