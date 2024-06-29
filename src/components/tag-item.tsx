ca5e96815e9004c755eae6e34035f11093a92e1c
hongducdev <contact.hongduc@gmail.com>
2024-06-29 12:21:09 +0700

    chore: Update TagItem component to handle active state
    Refactor the TagItem component to include an `isActive` prop and update the class names based on the active state. This allows for proper styling of the tag item based on its active state.

--- src/components/tag-item.tsx
+++ src/components/tag-item.tsx
@@ -1,11 +1,15 @@
 import { Tag } from "@prisma/client";
 import Link from "next/link";
 
-const TagItem = ({ tag }: { tag: Tag }) => {
+const TagItem = ({ tag, isActive }: { tag: Tag; isActive: boolean }) => {
   return (
     <Link
       href={`/blogs/tags/${tag.tagName}`}
-      className="bg-zinc-600 text-zinc-100 px-2 rounded-full hover:bg-zinc-700"
+      className={`${
+        isActive
+          ? "bg-zinc-600 text-zinc-100 hover:bg-zinc-700"
+          : "text-zinc-700 hover:text-zinc-600 bg-zinc-300 hover:bg-zinc-400"
+      } px-2 rounded-full font-medium`}
     >
       # {tag.tagName}
     </Link>
