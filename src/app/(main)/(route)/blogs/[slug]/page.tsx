bb5864ec7a62317da721451d03b1b4c3f6c71ba7
hongducdev <contact.hongduc@gmail.com>
2024-07-13 19:18:01 +0700

    Remove unused code and comments in sitemap.ts and page.tsx

--- src/app/(main)/(route)/blogs/[slug]/page.tsx
+++ src/app/(main)/(route)/blogs/[slug]/page.tsx
@@ -1,145 +1,136 @@
-// import { getPublishedPosts, getSingleBlogPost } from "@/apis";
-// import { Post } from "@prisma/client";
-// import { CalendarClock } from "lucide-react";
-// import type { Metadata } from "next";
-// import Image from "next/image";
-// import { notFound } from "next/navigation";
-// import { Lexend } from "next/font/google";
-// import NavPageDetail from "@/components/nav-page-detail";
-// import Link from "next/link";
-// import MarkdownRender from "@/components/markdown-render";
-// import CommentInput from "@/components/comment/comment-input";
+import { getPublishedPosts, getSingleBlogPost } from "@/apis";
+import { Post } from "@prisma/client";
+import { CalendarClock } from "lucide-react";
+import type { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { Lexend } from "next/font/google";
+import NavPageDetail from "@/components/nav-page-detail";
+import Link from "next/link";
+import MarkdownRender from "@/components/markdown-render";
+import CommentInput from "@/components/comment/comment-input";
 
-// const lexand = Lexend({ subsets: ["latin"] });
+const lexand = Lexend({ subsets: ["latin"] });
 
-// export const revalidate = 60;
+export const revalidate = 60;
 
-// interface BlogPageProps {
-//   params: {
-//     slug: string;
-//   };
-// }
-
-// export const generateStaticParams = async () => {
-//   try {
-//     const posts: Post[] = await getPublishedPosts();
-//     return posts.map((post) => ({
-//       slug: post.slug,
-//     }));
-//   } catch (error) {
-//     console.error("Error fetching posts:", error);
-//     return [];
-//   }
-// };
-
-// export const generateMetadata = async ({
-//   params,
-// }: BlogPageProps): Promise<Metadata> => {
-//   try {
-//     const postPage = await getSingleBlogPost(params.slug);
-//     return {
-//       title: postPage.title,
-//       description: postPage.shortDesc,
-//       openGraph: {
-//         title: postPage.title,
-//         description: postPage.shortDesc,
-//         type: "book",
-//         url: `${process.env.BASE_URL}/blogs/${postPage.slug}`,
-//         releaseDate: new Date(postPage.createdAt).toISOString(),
-//         tags: postPage.tagName ? [postPage.tagName] : [],
-//         images: [
-//           {
-//             url: postPage.thumbnail,
-//             alt: postPage.title,
-//           },
-//         ],
-//       },
-//       alternates: {
-//         canonical: `${process.env.BASE_URL}/blogs/${postPage.slug}`,
-//       },
-//       robots: "index, follow",
-//     };
-//   } catch (error) {
-//     return {
-//       title: "Post not found",
-//       description: "The post you are looking for does not exist.",
-//     };
-//   }
-// };
-
-// const PostDetailPage = async ({ params }: BlogPageProps) => {
-//   try {
-//     const postPage = await getSingleBlogPost(params.slug);
-
-//     return (
-//       <div className={lexand.className}>
-//         <div className="flex flex-col">
-//           <div className="relative h-[25vh] w-full">
-//             <Image
-//               src={postPage.thumbnail}
-//               alt={postPage.title}
-//               fill
-//               className="object-cover rounded-xl"
-//             />
-//           </div>
-//           <div className="my-4">
-//             <NavPageDetail
-//               title={postPage.title}
-//               link={`${process.env.BASE_URL}/blog/${postPage.slug}`}
-//             />
-//           </div>
-
-//           <div className="w-full mx-auto">
-//             <h1 className="text-lg lg:text-3xl font-bold">{postPage.title}</h1>
-//             <p className="text-sm lg:text-base mt-2">{postPage.shortDesc}</p>
-//             <div className="flex items-center gap-2 text-base mt-2">
-//               <CalendarClock className="w-5 h-5" />
-//               <span>
-//                 Last updated:{" "}
-//                 {new Date(postPage.updatedAt).toLocaleDateString("en-US", {
-//                   year: "numeric",
-//                   month: "long",
-//                   day: "numeric",
-//                 })}
-//               </span>
-//             </div>
-//             <div className="mt-4">
-//               <Link
-//                 href={`/blogs/tags/${postPage.tagName}`}
-//                 className="bg-zinc-600 text-zinc-100 px-2 rounded-full hover:bg-zinc-700"
-//               >
-//                 # {postPage.tagName}
-//               </Link>
-//             </div>
-//           </div>
-//           <section className="max-w-2xl w-full mx-auto my-5">
-//             <article className="prose dark:prose-invert max-w-7xl w-full mx-auto text-black dark:text-white">
-//               <MarkdownRender mdString={postPage.content} />
-//             </article>
-//           </section>
-
-//           <div className="w-full mx-auto px-2 xl:px-0">
-//             <h3 className="text-xl font-semibold">Comments</h3>
-//             <div className="w-full h-[1px] bg-input"></div>
-//             <div className="mt-4">
-//               <CommentInput postId={postPage.id} slug={params.slug} />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   } catch (error) {
-//     notFound();
-//   }
-// };
-
-// export default PostDetailPage;
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
+interface BlogPageProps {
+  params: {
+    slug: string;
+  };
 }
 
-export default Page
+export const generateStaticParams = async () => {
+  try {
+    const posts: Post[] = await getPublishedPosts();
+    return posts.map((post) => ({
+      slug: post.slug,
+    }));
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
+};
+
+export const generateMetadata = async ({
+  params,
+}: BlogPageProps): Promise<Metadata> => {
+  try {
+    const postPage = await getSingleBlogPost(params.slug);
+    return {
+      title: postPage.title,
+      description: postPage.shortDesc,
+      openGraph: {
+        title: postPage.title,
+        description: postPage.shortDesc,
+        type: "book",
+        url: `${process.env.BASE_URL}/blogs/${postPage.slug}`,
+        releaseDate: new Date(postPage.createdAt).toISOString(),
+        tags: postPage.tagName ? [postPage.tagName] : [],
+        images: [
+          {
+            url: postPage.thumbnail,
+            alt: postPage.title,
+          },
+        ],
+      },
+      alternates: {
+        canonical: `${process.env.BASE_URL}/blogs/${postPage.slug}`,
+      },
+      robots: "index, follow",
+    };
+  } catch (error) {
+    return {
+      title: "Post not found",
+      description: "The post you are looking for does not exist.",
+    };
+  }
+};
+
+const PostDetailPage = async ({ params }: BlogPageProps) => {
+  try {
+    const postPage = await getSingleBlogPost(params.slug);
+
+    return (
+      <div className={lexand.className}>
+        <div className="flex flex-col">
+          <div className="relative h-[25vh] w-full">
+            <Image
+              src={postPage.thumbnail}
+              alt={postPage.title}
+              fill
+              className="object-cover rounded-xl"
+            />
+          </div>
+          <div className="my-4">
+            <NavPageDetail
+              title={postPage.title}
+              link={`${process.env.BASE_URL}/blog/${postPage.slug}`}
+            />
+          </div>
+
+          <div className="w-full mx-auto">
+            <h1 className="text-lg lg:text-3xl font-bold">{postPage.title}</h1>
+            <p className="text-sm lg:text-base mt-2">{postPage.shortDesc}</p>
+            <div className="flex items-center gap-2 text-base mt-2">
+              <CalendarClock className="w-5 h-5" />
+              <span>
+                Last updated:{" "}
+                {new Date(postPage.updatedAt).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+            </div>
+            <div className="mt-4">
+              <Link
+                href={`/blogs/tags/${postPage.tagName}`}
+                className="bg-zinc-600 text-zinc-100 px-2 rounded-full hover:bg-zinc-700"
+              >
+                # {postPage.tagName}
+              </Link>
+            </div>
+          </div>
+          <section className="max-w-2xl w-full mx-auto my-5">
+            <article className="prose dark:prose-invert max-w-7xl w-full mx-auto text-black dark:text-white">
+              <MarkdownRender mdString={postPage.content} />
+            </article>
+          </section>
+
+          <div className="w-full mx-auto px-2 xl:px-0">
+            <h3 className="text-xl font-semibold">Comments</h3>
+            <div className="w-full h-[1px] bg-input"></div>
+            <div className="mt-4">
+              <CommentInput postId={postPage.id} slug={params.slug} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    notFound();
+  }
+};
+
+export default PostDetailPage;
--- src/app/(main)/(route)/dashboard/post/page.tsx
+++ src/app/(main)/(route)/dashboard/post/page.tsx
@@ -1,125 +1,116 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import { Button } from "@/components/ui/button";
-// import { Post } from "@prisma/client";
-// import Link from "next/link";
-// import {
-//   Table,
-//   TableBody,
-//   TableCaption,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/table";
-// import {
-//   AlertDialog,
-//   AlertDialogAction,
-//   AlertDialogCancel,
-//   AlertDialogContent,
-//   AlertDialogDescription,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogTitle,
-//   AlertDialogTrigger,
-// } from "@/components/ui/alert-dialog";
-// import Image from "next/image";
-// import { Pencil, Trash2 } from "lucide-react";
+"use client";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Post } from "@prisma/client";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { Pencil, Trash2 } from "lucide-react";
 
-// const PostDashboardPage = () => {
-//   const [posts, setPosts] = useState<Post[]>([]);
+const PostDashboardPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
 
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//       const response = await fetch("/api/posts");
-//       const data = await response.json();
-//       if (response.status === 200) {
-//         setPosts(data);
-//       }
-//     };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts");
+      const data = await response.json();
+      if (response.status === 200) {
+        setPosts(data);
+      }
+    };
 
-//     fetchPosts();
-//   }, []);
+    fetchPosts();
+  }, []);
 
-//   return (
-//     <div>
-//       <Button>
-//         <Link href="/dashboard/post/create">Create Post</Link>
-//       </Button>
-//       <div className="">
-//         <Table>
-//           <TableCaption>A list of your recent invoices.</TableCaption>
-//           <TableHeader>
-//             <TableRow>
-//               <TableHead className="w-[100px]">STT</TableHead>
-//               <TableHead>Thumbnail</TableHead>
-//               <TableHead>Tiêu đề</TableHead>
-//               <TableHead>Tác vụ</TableHead>
-//             </TableRow>
-//           </TableHeader>
-//           <TableBody>
-//             {posts.map((post, index) => (
-//               <TableRow key={post.id}>
-//                 <TableCell>{index + 1}</TableCell>
-//                 <TableCell>
-//                   <Image
-//                     src={post.thumbnail}
-//                     alt={post.title}
-//                     width="150"
-//                     height="50"
-//                     className="object-cover rounded-lg"
-//                   />
-//                 </TableCell>
-//                 <TableCell>{post.title}</TableCell>
-//                 <TableCell>
-//                   <div className="flex items-center space-x-3">
-//                     <Button>
-//                       <Link href={`/dashboard/post/edit/${post.slug}`}>
-//                         <Pencil className="w-4 h-4" />
-//                       </Link>
-//                     </Button>
-
-//                     <AlertDialog>
-//                       <AlertDialogTrigger>
-//                         <Button variant="destructive">
-//                           <Trash2 className="w-4 h-4" />
-//                         </Button>
-//                       </AlertDialogTrigger>
-//                       <AlertDialogContent>
-//                         <AlertDialogHeader>
-//                           <AlertDialogTitle>
-//                             Xác nhận xóa bài viết {post.title}
-//                           </AlertDialogTitle>
-//                           <AlertDialogDescription>
-//                             Bạn có chắc chắn muốn xóa tag này không?
-//                           </AlertDialogDescription>
-//                         </AlertDialogHeader>
-//                         <AlertDialogFooter>
-//                           <AlertDialogCancel>Hủy</AlertDialogCancel>
-//                           <AlertDialogAction onClick={() => {}}>
-//                             Xóa
-//                           </AlertDialogAction>
-//                         </AlertDialogFooter>
-//                       </AlertDialogContent>
-//                     </AlertDialog>
-//                   </div>
-//                 </TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PostDashboardPage;
-import React from 'react'
-
-const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <Button>
+        <Link href="/dashboard/post/create">Create Post</Link>
+      </Button>
+      <div className="">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">STT</TableHead>
+              <TableHead>Thumbnail</TableHead>
+              <TableHead>Tiêu đề</TableHead>
+              <TableHead>Tác vụ</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {posts.map((post, index) => (
+              <TableRow key={post.id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>
+                  <Image
+                    src={post.thumbnail}
+                    alt={post.title}
+                    width="150"
+                    height="50"
+                    className="object-cover rounded-lg"
+                  />
+                </TableCell>
+                <TableCell>{post.title}</TableCell>
+                <TableCell>
+                  <div className="flex items-center space-x-3">
+                    <Button>
+                      <Link href={`/dashboard/post/edit/${post.slug}`}>
+                        <Pencil className="w-4 h-4" />
+                      </Link>
+                    </Button>
 
-export default Page
+                    <AlertDialog>
+                      <AlertDialogTrigger>
+                        <Button variant="destructive">
+                          <Trash2 className="w-4 h-4" />
+                        </Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Xác nhận xóa bài viết {post.title}
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            Bạn có chắc chắn muốn xóa tag này không?
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>Hủy</AlertDialogCancel>
+                          <AlertDialogAction onClick={() => {}}>
+                            Xóa
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default PostDashboardPage;
